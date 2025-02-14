e3ed3f80bcebc0661a4e4ea89be78c2be49b0169
David Acosta Fajardo <108051764+DavidAcostaF@users.noreply.github.com>
2025-02-14 09:47:43 -0700

    Initialize project structure and update UI components

--- app/src/main/java/acosta/david/popcornfactory/DetallePelicula.kt
+++ app/src/main/java/acosta/david/popcornfactory/DetallePelicula.kt
@@ -1,6 +1,8 @@
 package acosta.david.popcornfactory
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,14 @@ class DetallePelicula : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+
+        val bundle = intent.extras
+        if (bundle != null) {
+            findViewById<ImageView>(R.id.iv_pelicula_imagen).setImageResource(bundle.getInt("header"))
+            findViewById<TextView>(R.id.tv_nombre_pelicula).text = bundle.getString("titulo")
+            findViewById<TextView>(R.id.tv_pelicula_desc).text = bundle.getString("sinopsis")
         }
     }
 }
--- app/src/main/java/acosta/david/popcornfactory/Pelicula.kt
+++ app/src/main/java/acosta/david/popcornfactory/Pelicula.kt
@@ -1,4 +1,4 @@
 package acosta.david.popcornfactory
 
-data class Pelicula(val titulo:String,val imagen:Int,val header:Int,val sinopsis:String,val duracion:String) {
+data class Pelicula(val titulo:String,val image:Int,val header:Int,val sinopsis:String) {
 }
